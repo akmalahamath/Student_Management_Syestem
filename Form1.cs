9b5aa36b8f2b1e9132f9a21084e3a0d824a8d431
akmalrazak2005 <akmalrazak0403@gmail.com>
2026-08-16 09:46:16 +0530

    Dashboard Form Created.

--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Student_Management_Syestem
 {
-    public partial class Form1 : Form
+    public partial class Loginform : Form
     {
-        public Form1()
+        public Loginform()
         {
             InitializeComponent();
         }
