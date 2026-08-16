b752c818cc17b6a00b9bdebc9dbefa0805ce2368
akmalrazak2005 <akmalrazak0403@gmail.com>
2026-08-16 16:03:59 +0530

    Login Ui Color Changes.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,6 +48,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
             this.label1.Location = new System.Drawing.Point(288, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(496, 38);
@@ -57,6 +58,7 @@
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label3);
@@ -77,6 +79,7 @@
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Navy;
             this.button1.Location = new System.Drawing.Point(87, 275);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(314, 72);
@@ -131,6 +134,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Green;
             this.label4.Location = new System.Drawing.Point(77, 486);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(203, 25);
--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -32,18 +32,18 @@
             this.lblWelcomeTitle = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblStudentCount = new System.Windows.Forms.Label();
             this.lblStudentTitle = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblCourseCount = new System.Windows.Forms.Label();
             this.lblCourseTitle = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblEntrollmentCount = new System.Windows.Forms.Label();
             this.lblEntrollmentTitle = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblPaymentCount = new System.Windows.Forms.Label();
             this.lblPaymentTitle = new System.Windows.Forms.Label();
             this.lbldashboard = new System.Windows.Forms.Label();
-            this.lblStudentCount = new System.Windows.Forms.Label();
-            this.lblCourseCount = new System.Windows.Forms.Label();
-            this.lblEntrollmentCount = new System.Windows.Forms.Label();
-            this.lblPaymentCount = new System.Windows.Forms.Label();
             this.btnStudents = new System.Windows.Forms.Button();
             this.btnCourses = new System.Windows.Forms.Button();
             this.btnEnrollment = new System.Windows.Forms.Button();
@@ -90,35 +90,16 @@
             this.panel1.TabIndex = 4;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // panel2
+            // lblStudentCount
             // 
-            this.panel2.BackColor = System.Drawing.Color.LimeGreen;
-            this.panel2.Controls.Add(this.lblCourseCount);
-            this.panel2.Controls.Add(this.lblCourseTitle);
-            this.panel2.Location = new System.Drawing.Point(678, 168);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(240, 137);
-            this.panel2.TabIndex = 5;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.LimeGreen;
-            this.panel3.Controls.Add(this.lblEntrollmentCount);
-            this.panel3.Controls.Add(this.lblEntrollmentTitle);
-            this.panel3.Location = new System.Drawing.Point(343, 346);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(243, 140);
-            this.panel3.TabIndex = 5;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.LimeGreen;
-            this.panel4.Controls.Add(this.lblPaymentCount);
-            this.panel4.Controls.Add(this.lblPaymentTitle);
-            this.panel4.Location = new System.Drawing.Point(678, 346);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(240, 137);
-            this.panel4.TabIndex = 6;
+            this.lblStudentCount.AutoSize = true;
+            this.lblStudentCount.Font = new System.Drawing.Font("Segoe UI Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudentCount.ForeColor = System.Drawing.Color.Navy;
+            this.lblStudentCount.Location = new System.Drawing.Point(78, 71);
+            this.lblStudentCount.Name = "lblStudentCount";
+            this.lblStudentCount.Size = new System.Drawing.Size(92, 45);
+            this.lblStudentCount.TabIndex = 16;
+            this.lblStudentCount.Text = "1500";
             // 
             // lblStudentTitle
             // 
@@ -130,6 +111,27 @@
             this.lblStudentTitle.TabIndex = 15;
             this.lblStudentTitle.Text = "TOTAL STUDENTS";
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LimeGreen;
+            this.panel2.Controls.Add(this.lblCourseCount);
+            this.panel2.Controls.Add(this.lblCourseTitle);
+            this.panel2.Location = new System.Drawing.Point(678, 168);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(240, 137);
+            this.panel2.TabIndex = 5;
+            // 
+            // lblCourseCount
+            // 
+            this.lblCourseCount.AutoSize = true;
+            this.lblCourseCount.Font = new System.Drawing.Font("Segoe UI Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCourseCount.ForeColor = System.Drawing.Color.Navy;
+            this.lblCourseCount.Location = new System.Drawing.Point(78, 68);
+            this.lblCourseCount.Name = "lblCourseCount";
+            this.lblCourseCount.Size = new System.Drawing.Size(92, 45);
+            this.lblCourseCount.TabIndex = 17;
+            this.lblCourseCount.Text = "1500";
+            // 
             // lblCourseTitle
             // 
             this.lblCourseTitle.AutoSize = true;
@@ -140,6 +142,27 @@
             this.lblCourseTitle.TabIndex = 16;
             this.lblCourseTitle.Text = "TOTAL COURSES";
             // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.LimeGreen;
+            this.panel3.Controls.Add(this.lblEntrollmentCount);
+            this.panel3.Controls.Add(this.lblEntrollmentTitle);
+            this.panel3.Location = new System.Drawing.Point(343, 346);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(243, 140);
+            this.panel3.TabIndex = 5;
+            // 
+            // lblEntrollmentCount
+            // 
+            this.lblEntrollmentCount.AutoSize = true;
+            this.lblEntrollmentCount.Font = new System.Drawing.Font("Segoe UI Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEntrollmentCount.ForeColor = System.Drawing.Color.Navy;
+            this.lblEntrollmentCount.Location = new System.Drawing.Point(78, 72);
+            this.lblEntrollmentCount.Name = "lblEntrollmentCount";
+            this.lblEntrollmentCount.Size = new System.Drawing.Size(92, 45);
+            this.lblEntrollmentCount.TabIndex = 18;
+            this.lblEntrollmentCount.Text = "1500";
+            // 
             // lblEntrollmentTitle
             // 
             this.lblEntrollmentTitle.AutoSize = true;
@@ -149,6 +172,27 @@
             this.lblEntrollmentTitle.Size = new System.Drawing.Size(206, 32);
             this.lblEntrollmentTitle.TabIndex = 17;
             this.lblEntrollmentTitle.Text = "ENTROLLMENTS";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.LimeGreen;
+            this.panel4.Controls.Add(this.lblPaymentCount);
+            this.panel4.Controls.Add(this.lblPaymentTitle);
+            this.panel4.Location = new System.Drawing.Point(678, 346);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(240, 137);
+            this.panel4.TabIndex = 6;
+            // 
+            // lblPaymentCount
+            // 
+            this.lblPaymentCount.AutoSize = true;
+            this.lblPaymentCount.Font = new System.Drawing.Font("Segoe UI Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPaymentCount.ForeColor = System.Drawing.Color.Navy;
+            this.lblPaymentCount.Location = new System.Drawing.Point(81, 67);
+            this.lblPaymentCount.Name = "lblPaymentCount";
+            this.lblPaymentCount.Size = new System.Drawing.Size(92, 45);
+            this.lblPaymentCount.TabIndex = 19;
+            this.lblPaymentCount.Text = "1500";
             // 
             // lblPaymentTitle
             // 
@@ -169,50 +213,6 @@
             this.lbldashboard.Size = new System.Drawing.Size(256, 48);
             this.lbldashboard.TabIndex = 7;
             this.lbldashboard.Text = "DASHBOARD";
-            // 
-            // lblStudentCount
-            // 
-            this.lblStudentCount.AutoSize = true;
-            this.lblStudentCount.Font = new System.Drawing.Font("Segoe UI Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStudentCount.ForeColor = System.Drawing.Color.Navy;
-            this.lblStudentCount.Location = new System.Drawing.Point(78, 71);
-            this.lblStudentCount.Name = "lblStudentCount";
-            this.lblStudentCount.Size = new System.Drawing.Size(92, 45);
-            this.lblStudentCount.TabIndex = 16;
-            this.lblStudentCount.Text = "1500";
-            // 
-            // lblCourseCount
-            // 
-            this.lblCourseCount.AutoSize = true;
-            this.lblCourseCount.Font = new System.Drawing.Font("Segoe UI Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCourseCount.ForeColor = System.Drawing.Color.Navy;
-            this.lblCourseCount.Location = new System.Drawing.Point(78, 68);
-            this.lblCourseCount.Name = "lblCourseCount";
-            this.lblCourseCount.Size = new System.Drawing.Size(92, 45);
-            this.lblCourseCount.TabIndex = 17;
-            this.lblCourseCount.Text = "1500";
-            // 
-            // lblEntrollmentCount
-            // 
-            this.lblEntrollmentCount.AutoSize = true;
-            this.lblEntrollmentCount.Font = new System.Drawing.Font("Segoe UI Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEntrollmentCount.ForeColor = System.Drawing.Color.Navy;
-            this.lblEntrollmentCount.Location = new System.Drawing.Point(78, 72);
-            this.lblEntrollmentCount.Name = "lblEntrollmentCount";
-            this.lblEntrollmentCount.Size = new System.Drawing.Size(92, 45);
-            this.lblEntrollmentCount.TabIndex = 18;
-            this.lblEntrollmentCount.Text = "1500";
-            // 
-            // lblPaymentCount
-            // 
-            this.lblPaymentCount.AutoSize = true;
-            this.lblPaymentCount.Font = new System.Drawing.Font("Segoe UI Black", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPaymentCount.ForeColor = System.Drawing.Color.Navy;
-            this.lblPaymentCount.Location = new System.Drawing.Point(81, 67);
-            this.lblPaymentCount.Name = "lblPaymentCount";
-            this.lblPaymentCount.Size = new System.Drawing.Size(92, 45);
-            this.lblPaymentCount.TabIndex = 19;
-            this.lblPaymentCount.Text = "1500";
             // 
             // btnStudents
             // 
@@ -332,7 +332,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.DarkGreen;
-            this.label1.Location = new System.Drawing.Point(529, 527);
+            this.label1.Location = new System.Drawing.Point(527, 527);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(249, 32);
             this.label1.TabIndex = 15;
