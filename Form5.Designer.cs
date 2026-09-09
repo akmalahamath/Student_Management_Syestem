885632bfb6cb2f071a82a157b07ef925762cc4da
aliffzeenathzahara-create <aliffzeenathzahara@gmail.com>
2026-09-09 17:27:56 +0530

    Form size setup

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(975, 744);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(997, 800);
+            this.MinimumSize = new System.Drawing.Size(997, 800);
+            this.Name = "Form5";
             this.Text = "Form5";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
