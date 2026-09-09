9d1d974f9940aefd714d0f141546d7d832324879
aliffzeenathzahara-create <aliffzeenathzahara@gmail.com>
2026-09-09 17:52:33 +0530

    Label for the 'course' created

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -85,7 +85,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(193, 357);
+            this.label4.Location = new System.Drawing.Point(225, 357);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(91, 29);
             this.label4.TabIndex = 5;
