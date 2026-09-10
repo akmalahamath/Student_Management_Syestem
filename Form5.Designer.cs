ec5e3d4a77e5ced2c32babfc08c0328554d3e8b6
aliffzeenathzahara-create <aliffzeenathzahara@gmail.com>
2026-09-10 16:25:21 +0530

    Form size error handling

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -52,7 +52,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(381, 66);
+            this.label1.Location = new System.Drawing.Point(345, 64);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(303, 46);
             this.label1.TabIndex = 0;
@@ -242,8 +242,8 @@
             // 
             // Form5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(975, 744);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
