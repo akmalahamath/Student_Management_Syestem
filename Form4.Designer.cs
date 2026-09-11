c9976e3a40dfd0803e1b994b5b1c90cd7ae07f65
akmalahamath <akmalahamath0403@gmail.com>
2026-09-11 14:25:07 +0530

    UI Changes

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -39,8 +39,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
@@ -153,8 +151,6 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.ForestGreen;
             this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.textBox5);
             this.panel1.Controls.Add(this.label7);
@@ -175,43 +171,21 @@
             // 
             this.button4.BackColor = System.Drawing.Color.Violet;
             this.button4.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(307, 501);
+            this.button4.Location = new System.Drawing.Point(217, 501);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(83, 62);
+            this.button4.Size = new System.Drawing.Size(121, 62);
             this.button4.TabIndex = 27;
             this.button4.Text = "CLEAR";
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.Violet;
-            this.button3.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(205, 501);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(83, 62);
-            this.button3.TabIndex = 26;
-            this.button3.Text = "DELETE";
-            this.button3.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Violet;
-            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(109, 501);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(83, 62);
-            this.button2.TabIndex = 25;
-            this.button2.Text = "UPDATE";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Violet;
             this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(11, 501);
+            this.button1.Location = new System.Drawing.Point(49, 501);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 62);
+            this.button1.Size = new System.Drawing.Size(129, 62);
             this.button1.TabIndex = 24;
             this.button1.Text = "ADD";
             this.button1.UseVisualStyleBackColor = false;
@@ -432,8 +406,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label8;
     }
