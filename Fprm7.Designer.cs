c653b3c73209119958ab281430f2dea0f2712996
akmalahamath <akmalahamath0403@gmail.com>
2026-09-12 21:50:39 +0530

    All error handling

--- Fprm7.Designer.cs
+++ Fprm7.Designer.cs
@@ -49,9 +49,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(62, 43);
+            this.label1.Location = new System.Drawing.Point(70, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(473, 42);
+            this.label1.Size = new System.Drawing.Size(567, 52);
             this.label1.TabIndex = 0;
             this.label1.Text = "COURSE MANAGEMENT";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -60,9 +60,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(64, 131);
+            this.label2.Location = new System.Drawing.Point(72, 164);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.Size = new System.Drawing.Size(129, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "Course ID";
             // 
@@ -70,9 +70,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(64, 189);
+            this.label3.Location = new System.Drawing.Point(72, 236);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(144, 25);
+            this.label3.Size = new System.Drawing.Size(173, 29);
             this.label3.TabIndex = 2;
             this.label3.Text = "Course Name";
             // 
@@ -80,9 +80,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(64, 245);
+            this.label4.Location = new System.Drawing.Point(72, 306);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(140, 25);
+            this.label4.Size = new System.Drawing.Size(167, 29);
             this.label4.TabIndex = 3;
             this.label4.Text = "Course Code";
             // 
@@ -90,9 +90,9 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(64, 300);
+            this.label5.Location = new System.Drawing.Point(72, 375);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(93, 25);
+            this.label5.Size = new System.Drawing.Size(111, 29);
             this.label5.TabIndex = 4;
             this.label5.Text = "Duration";
             // 
@@ -100,9 +100,9 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(64, 367);
+            this.label6.Location = new System.Drawing.Point(72, 459);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(102, 25);
+            this.label6.Size = new System.Drawing.Size(121, 29);
             this.label6.TabIndex = 5;
             this.label6.Text = "Instructor";
             // 
@@ -110,62 +110,69 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(64, 424);
+            this.label7.Location = new System.Drawing.Point(72, 530);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(120, 25);
+            this.label7.Size = new System.Drawing.Size(146, 29);
             this.label7.TabIndex = 6;
             this.label7.Text = "Description";
             // 
             // maskedTextBox1
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(243, 134);
+            this.maskedTextBox1.Location = new System.Drawing.Point(273, 168);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(257, 22);
+            this.maskedTextBox1.Size = new System.Drawing.Size(289, 26);
             this.maskedTextBox1.TabIndex = 7;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(243, 192);
+            this.textBox1.Location = new System.Drawing.Point(273, 240);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(257, 22);
+            this.textBox1.Size = new System.Drawing.Size(289, 26);
             this.textBox1.TabIndex = 8;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(243, 248);
+            this.textBox2.Location = new System.Drawing.Point(273, 310);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(257, 22);
+            this.textBox2.Size = new System.Drawing.Size(289, 26);
             this.textBox2.TabIndex = 9;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(243, 300);
+            this.textBox3.Location = new System.Drawing.Point(273, 375);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(257, 22);
+            this.textBox3.Size = new System.Drawing.Size(289, 26);
             this.textBox3.TabIndex = 10;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(243, 367);
+            this.textBox4.Location = new System.Drawing.Point(273, 459);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(257, 22);
+            this.textBox4.Size = new System.Drawing.Size(289, 26);
             this.textBox4.TabIndex = 11;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(243, 424);
+            this.textBox5.Location = new System.Drawing.Point(273, 530);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(257, 22);
+            this.textBox5.Size = new System.Drawing.Size(289, 26);
             this.textBox5.TabIndex = 12;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.GreenYellow;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(243, 496);
+            this.button1.Location = new System.Drawing.Point(273, 620);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 50);
+            this.button1.Size = new System.Drawing.Size(125, 62);
             this.button1.TabIndex = 13;
             this.button1.Text = "ADD";
             this.button1.UseVisualStyleBackColor = false;
@@ -174,19 +181,20 @@
             // 
             this.button2.BackColor = System.Drawing.Color.GreenYellow;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(390, 496);
+            this.button2.Location = new System.Drawing.Point(439, 620);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 50);
+            this.button2.Size = new System.Drawing.Size(124, 62);
             this.button2.TabIndex = 14;
             this.button2.Text = "CLEAR";
             this.button2.UseVisualStyleBackColor = false;
             // 
-            // Form6
+            // Fprm7
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.ClientSize = new System.Drawing.Size(870, 602);
+            this.ClientSize = new System.Drawing.Size(974, 741);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox5);
@@ -202,10 +210,11 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(888, 649);
-            this.MinimumSize = new System.Drawing.Size(888, 649);
-            this.Name = "Form6";
+            this.MaximumSize = new System.Drawing.Size(996, 797);
+            this.MinimumSize = new System.Drawing.Size(996, 797);
+            this.Name = "Fprm7";
             this.Text = "Form6";
             this.ResumeLayout(false);
             this.PerformLayout();
