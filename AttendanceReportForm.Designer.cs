a7c5ee206df30bb6b310b1695edb1a27f6b53641
akmalahamath <akmalahamath0403@gmail.com>
2026-09-14 01:45:13 +0530

    Chnages Overall

--- AttendanceReportForm.Designer.cs
+++ AttendanceReportForm.Designer.cs
@@ -21,12 +21,15 @@ namespace Student_Management_Syestem
             // 
             // AttendanceReportForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AliceBlue;
             this.ClientSize = new System.Drawing.Size(975, 744);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(997, 800);
+            this.MinimumSize = new System.Drawing.Size(997, 800);
             this.Name = "AttendanceReportForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Attendance Report";
--- CourseReportForm.Designer.cs
+++ CourseReportForm.Designer.cs
@@ -32,10 +32,14 @@ namespace Student_Management_Syestem
             // 
             // CourseReportForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PowderBlue;
             this.ClientSize = new System.Drawing.Size(975, 744);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(997, 800);
+            this.MinimumSize = new System.Drawing.Size(997, 800);
             this.Name = "CourseReportForm";
             this.Text = "CourseReportForm";
             this.ResumeLayout(false);
